2917c9616dcff1232e0e57d340a3b5ffe31b1eee
bauner <bauner@gmx.de>
2014-09-12 18:03:09 +0200

    higher thermal throttle

--- arch/arm/mach-exynos/mach-px.c
+++ arch/arm/mach-exynos/mach-px.c
@@ -7011,14 +7011,14 @@ static struct platform_device *smdkc210_devices[] __initdata = {
 /* below temperature base on the celcius degree */
 struct s5p_platform_tmu px_tmu_data __initdata = {
 	.ts = {
-		.stop_1st_throttle  = 61,
-		.start_1st_throttle = 64,
-		.stop_2nd_throttle  = 87,
-		.start_2nd_throttle = 103,
+		.stop_1st_throttle  = 71,
+		.start_1st_throttle = 74,
+		.stop_2nd_throttle  = 90,
+		.start_2nd_throttle = 105,
 		.start_tripping     = 110,
 		.start_emergency    = 120,
-		.stop_mem_throttle  = 80,
-		.start_mem_throttle = 85,
+		.stop_mem_throttle  = 85,
+		.start_mem_throttle = 90,
 	},
 	.cpufreq = {
 		.limit_1st_throttle  = 800000, /* 800MHz in KHz order */
