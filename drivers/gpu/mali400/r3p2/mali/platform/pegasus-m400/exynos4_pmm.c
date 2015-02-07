02605da4c039b0f2c37ed1ad5507d25a714347ff
bauner <bauner@gmx.de>
2015-02-07 09:49:40 +0100

    4210 mali400 160mhz step0 +uv

--- drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
+++ drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
@@ -98,8 +98,8 @@ mali_dvfs_table mali_dvfs[MALI_DVFS_STEPS]={
 			/* step 3 */{440  ,1000000	,1025000   ,85   , 90},
 			/* step 4 */{533  ,1000000	,1075000   ,95   ,100} };
 #else
-			/* step 0 */{134  ,1000000	, 950000   ,85   , 90},
-			/* step 1 */{267  ,1000000	,1050000   ,85   ,100} };
+			/* step 0 */{160  ,1000000	, 950000   ,0    ,85},
+			/* step 1 */{267  ,1000000	,1000000   ,75   ,100} };
 #endif
 
 #ifdef EXYNOS4_ASV_ENABLED
@@ -173,9 +173,9 @@ static unsigned int asv_3d_volt_4210_12_table[MALI_DVFS_STEPS][ASV_LEVEL_4210_12
 };
 
 static unsigned int asv_3d_volt_4210_14_table[MALI_DVFS_STEPS][ASV_LEVEL_4210_14] = {
-	{  1000000,  1000000,   950000,   950000,   950000},	/* L1(134Mhz) */
+	{  1000000,  1000000,   900000,   900000,   900000},	/* L1(160Mhz) */
 #if (MALI_DVFS_STEPS > 1)
-	{  1100000,  1100000,  1000000,  1000000,   950000},	/* L0(266Mhz) */
+	{  1100000,  1100000,   950000,   950000,   950000},	/* L0(267Mhz) */
 #endif
 };
 #endif
